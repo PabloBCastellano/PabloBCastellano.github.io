79e937b2c14624ea4eaab9576eb9ae5f05d515a7
pbc2018 <pablo.branuelas@campusfp.es>
2020-02-19 19:14:32 +0100

    Update Funcionalidades.js

--- ProyectoBBDD/Funcionalidades.js
+++ ProyectoBBDD/Funcionalidades.js
@@ -1,3 +1,17 @@
 function Descargar(){
 	location.href="Gestor_Base_Datos.zip"
+	document.getElementById("Instrucciones").innerHTML="Las librerias necesarias para "+
+	" que funcione son:<br>"+
+	"<ul style='margin-left:2%;'>"+
+	"<li>pyqt5 </li>"+
+	"<li>pyodbc </li>"+
+	"<li>fdb </li>"+
+	"<li>psycopg2 </li>"+
+	"<li>fpdf </li>"+
+	"<li>pandas </li>"+
+	"<li>numpy </li>"+
+	"<li>seaborn </li>"+
+	"<li>xlrd </li>"+
+	"<li>pillow </li>"+
+	"<li>matplotlib </li>"+
 }
