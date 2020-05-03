1f2e30061395df50799f4308ea79544ec48a83a1
PabloBCastellano <46810121+PabloBCastellano@users.noreply.github.com>
2020-05-03 04:56:20 +0200

    Update Descargas.js

--- ProyectoBBDD/Descargas.js
+++ ProyectoBBDD/Descargas.js
@@ -1,3 +1,21 @@
 function Bajar_Documento(){
-	"<input type='password'/>"
+	var clave=prompt("Que clave tienes de acceso para descargar la documentacion:\n");
+	
+	if(clave=="DAMPBC2020DOCUM"){
+		window.open("Memoria_Proyecto.pdf");
+	}
+	else{
+		alert("No puedes acceder sin una contraseña");
+	}	
+}
+
+function Bajar_Proyecto(){
+var clave=prompt("Que clave tienes de acceso para descargar el proyecto n:\n");
+	
+	if(clave=="DAMPBC2020Project"){
+		window.open("Gestor_Base_Datos.zip","_blank");
+	}
+	else{
+		alert("No puedes acceder sin una contraseña");
+	}	
 }
